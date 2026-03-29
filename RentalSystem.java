03b44e0ef0d903696d18f1b2efbddf5d2d01e912
Greg T. Student <gregoryturcotte@trentu.ca>
2026-03-28 20:31:30 -0400

    completed task 1 3. added respective comments in code how it works

--- RentalSystem.java
+++ RentalSystem.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
+import java.io.BufferedWriter; //had to import for the actual text document to work did some minor testing to ensure it worked then deleted txt files prior to commit 
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RentalSystem {
     private static RentalSystem instance;
@@ -22,12 +25,45 @@ public class RentalSystem {
         return instance;
     }
 
+    // for saveVehicle since we are writing to a txt file need to include an actual try and catch otherwise won't compile similar for other methods 
+    private void saveVehicle(Vehicle vehicle) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("vehicles.txt", true))) {
+            writer.write(vehicle.getLicensePlate() + "," + vehicle.getMake() + "," + 
+                         vehicle.getModel() + "," + vehicle.getYear() + "," + vehicle.getStatus());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving vehicle.");
+        }
+    }
+
+   
+    private void saveCustomer(Customer customer) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("customers.txt", true))) {
+            writer.write(customer.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving customer.");
+        }
+    }
+
+    
+    private void saveRecord(RentalRecord record) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("rental_records.txt", true))) {
+            writer.write(record.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving record.");
+        }
+    }
+
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
+        saveVehicle(vehicle); 
     }
 
     public void addCustomer(Customer customer) {
         customers.add(customer);
+        saveCustomer(customer);
     }
 
     public Vehicle findVehicleByPlate(String plate) {
@@ -48,7 +84,22 @@ public class RentalSystem {
         if (v instanceof Rentable) {
             ((Rentable) v).rentVehicle();
             // Similar to below I put a 5th return aspect which was giving some confusion to me and still need to test. But if works this comment will be removed in future
-            rentalHistory.add(new RentalRecord(v, c, date, amount, "RENT"));
+            RentalRecord record = new RentalRecord(v, c, date, amount, "RENT");
+            rentalHistory.add(record);
+            
+            // This is where we call our new method to write to txt 
+            saveRecord(record);
+
+            //this part accomplishes the one goal of being asked to update to ensure full list in vehciles.txt 
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("vehicles.txt"))) {
+                for (Vehicle vehicle : vehicles) {
+                    writer.write(vehicle.getLicensePlate() + "," + vehicle.getMake() + "," + 
+                                 vehicle.getModel() + "," + vehicle.getYear() + "," + vehicle.getStatus());
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Error updating vehicles file.");
+            }
         }
     }
 
@@ -56,7 +107,22 @@ public class RentalSystem {
         if (v instanceof Rentable) {
             ((Rentable) v).returnVehicle();
             //struggled with the below but I think it works for most part? Will do testing post commit 
-            rentalHistory.add(new RentalRecord(v, c, date, fees, "RETURN"));
+            RentalRecord record = new RentalRecord(v, c, date, fees, "RETURN");
+            rentalHistory.add(record);
+            
+            // again similar to above 
+            saveRecord(record);
+
+            //identical to above but required to ensure meet that it is always up to date list in vehicles.txt 
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("vehicles.txt"))) {
+                for (Vehicle vehicle : vehicles) {
+                    writer.write(vehicle.getLicensePlate() + "," + vehicle.getMake() + "," + 
+                                 vehicle.getModel() + "," + vehicle.getYear() + "," + vehicle.getStatus());
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Error updating vehicles file.");
+            }
         }
     }
 
