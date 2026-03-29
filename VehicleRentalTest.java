4d30fd348a5780399135d0a4b828f5dd9e63c71e
Greg T. Student <gregoryturcotte@trentu.ca>
2026-03-29 17:03:02 -0400

    accidently forgot assignent included specific plates and I just used ones from task1 so modified those quickly and this should be final part for task2-1

--- VehicleRentalTest.java
+++ VehicleRentalTest.java
@@ -10,13 +10,13 @@ public class VehicleRentalTest {
         Car testVehicle= new Car("Toyota", "Corolla", 2011, 5); //used my personal vehicle for this :) 
 
         // Testing valid plates as described in task 2 
-        assertDoesNotThrow(() -> testVehicle.setLicensePlate("AAA111"));
-        assertEquals("AAA111", testVehicle.getLicensePlate());
-        assertDoesNotThrow(() -> testVehicle.setLicensePlate("BBB222"));
-        assertDoesNotThrow(() -> testVehicle.setLicensePlate("CCC333"));
+        assertDoesNotThrow(() -> testVehicle.setLicensePlate("AAA100"));
+        assertEquals("AAA100", testVehicle.getLicensePlate()); //realized I mistyped plate here so had to correct that to AAA111
+        assertDoesNotThrow(() -> testVehicle.setLicensePlate("ABC567"));
+        assertDoesNotThrow(() -> testVehicle.setLicensePlate("ZZZ999"));
         assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("")); //test for empty string
         assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate(null)); //test for null
-        assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("AAA1111")); // Test case for too long 
-        assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("BBB22"));   //test case for too short of a plate
+        assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("AAA1000")); // Test case for too long 
+        assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("ZZZ99"));   //test case for too short of a plate
     }
 }
