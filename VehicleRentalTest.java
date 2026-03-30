f88a4bda95ae94be279f9b2a3cd54524cbb16bc2
Greg T. Student <gregoryturcotte@trentu.ca>
2026-03-29 23:00:17 -0400

    realized all test should be in same class so did that and tweaked a few final things

--- VehicleRentalTest.java
+++ VehicleRentalTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
 
 
 public class VehicleRentalTest {
     @Test
     void testLicensePlate() {
-        //create a car to test the plate(s) 
+       
         Car testVehicle= new Car("Toyota", "Corolla", 2011, 5); //used my personal vehicle for this :) 
 
         // Testing valid plates as described in task 2 
@@ -19,5 +22,50 @@ public class VehicleRentalTest {
         assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("AAA1000")); // Test case for too long 
         assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("ZZZ99"));   //test case for too short of a plate
     }
-    
+    public class testSingletonRentalSystem {
+
+    	@Test
+        void testSingletonRentalSystem() throws Exception {
+          
+            Constructor<RentalSystem> constructor = RentalSystem.class.getDeclaredConstructor(); //setting up necessary constructor 
+
+          
+            int modifiers = constructor.getModifiers(); //get the modifiers 
+
+          
+            assertTrue(Modifier.isPrivate(modifiers), "This should ensure that the constructor remains private :)");
+            
+            RentalSystem instance = RentalSystem.getInstance(); //use this to obtain instance and insure not null 
+            assertNotNull(instance, "RentalSystem.getInstance() should not return null");
+
+        }
+    }
+    public class testLicensePlate{
+    	@Test
+    	void testRentAndReturnVehicle() {
+    	    Car car = new Car("Honda", "CRV", 2019, 5); //my gf car :)
+    	    car.setLicensePlate("BOO413");
+    	    Customer customer = new Customer(007, "Steve French");
+
+    	    assertEquals(Vehicle.VehicleStatus.Available, car.getStatus(), "Vehicle should begin as available state");
+    	    RentalSystem system = RentalSystem.getInstance(); //getting the actual instance 
+    	    
+    	    boolean rentResult = system.rentVehicle(car, customer, null, 100.0); //testing rental result 
+    	    
+    	    assertTrue(rentResult, "we should see true here to know its successful");
+    	    assertEquals(Vehicle.VehicleStatus.Rented, car.getStatus(), "should be updated to rented");
+
+
+    	    boolean doubleRentResult = system.rentVehicle(car, customer, null, 100.0);
+    	    assertFalse(doubleRentResult, "should not be rentable ");
+
+    	    boolean returnResult = system.returnVehicle(car, customer, null, 0.0); //test for actual return of vehicle 
+    	    assertTrue(returnResult, "should then show as true, for successful return");
+    	    assertEquals(Vehicle.VehicleStatus.Available, car.getStatus(), "vehicle should go back to available");
+
+    	  
+    	    boolean returnAgainResult = system.returnVehicle(car, customer, null, 0.0);  //trying to return same vehicle should result in fail aka false
+    	    assertFalse(returnAgainResult, "given we are returning here should expect false");
+    	}
+}
 }
